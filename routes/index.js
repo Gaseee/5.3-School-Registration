d60935cce34d7304c4ab5281b4a179c3a391d262
ll3m <ll3m@fresnou.org>
2022-02-02 08:48:22 -0800

    Completed Part 3 but courses/profile won't load in

--- routes/index.js
+++ routes/index.js
@@ -26,8 +26,10 @@ router.get('/students/add', studentController.renderAddForm);
 router.post('/students/add', studentController.addStudent);
 
 router.get('/students/delete/:id', studentController.deleteStudent);
-
 router.post('/students/:studentId/enroll/', studentController.enrollStudent);
 router.post('/students/:studentId/removeCourse/:courseId', studentController.removeCourse);
+
+router.post('/courses/:courseId/enroll/', courseController.enrollStudent);
+router.get('/courses/:courseId/removeStudent/:studentId', courseController.removeStudent);
 
 module.exports = router;
